c468e7eac586fcb2df9c110eed6e113b9eef4237
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-08-28 15:32:43 -0300

    config css global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Containers/Home";
+import About from "./Containers/About";
+import Resume from "./Containers/Resume";
+import Skils from "./Containers/Skils";
+import Portfolio from "./Containers/Portfolio";
+import Contact from "./Containers/Contact";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Routes>
+                <Route index path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Resume" element={<Resume />} />
+                <Route path="/Skils" element={<Skils />} />
+                <Route path="/Portfolio" element={<Portfolio />} />
+                <Route path="/Contact" element={<Contact />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
