eb8697c125abd185ca5099b19838a96ef354451d
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-08-31 15:48:46 -0300

    animacao do botao de contact na page home

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
+import "./style.scss";
 
 function Home() {
-  return (
-    <section id='home' className='home'>
-      <div className="home__text-wrapper">
-        <h1>
-          hello, I´m Isaias
-          <br />
-          Web Developer
-        </h1>
-      </div>
-    </section>
-  )
+    const navigate = useNavigate();
+
+    const handleNavigateContact = () => {
+        navigate("/contact");
+    };
+
+    return (
+        <section id="home" className="home">
+            <div className="home__text-wrapper">
+                <h1>
+                    hello, I´m Isaias
+                    <br />
+                    Web Developer
+                </h1>
+            </div>
+            <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform : 'translateY(550px)'
+            }}
+            end={{
+              transform : 'translateX(0px)'
+            }}
+            >
+                <div className="home__contact-me">
+                    <button onClick={handleNavigateContact}>hire me</button>
+                </div>
+            </Animate>
+        </section>
+    );
 }
 
-export default Home
+export default Home;
