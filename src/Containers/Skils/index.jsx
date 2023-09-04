d04ba33dc5009c26f8e02e22665ab5d1513bdf47
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-09-04 09:18:20 -0300

    finiche section my skills

--- src/Containers/Skils/index.jsx
+++ src/Containers/Skils/index.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
-import PageHeaderContent from '../../components/PageHeaderContainer'
-import {BsInfoCircleFill} from 'react-icons/bs'
+import React from "react";
+import PageHeaderContent from "../../components/PageHeaderContainer";
+import { BsInfoCircleFill } from "react-icons/bs";
+import { skilData } from "./utils";
+import { Animate } from "react-simple-animate";
+import { AnimateKeyframes } from "react-simple-animate";
+import { Line } from "rc-progress";
+import './style.scss'
 
 function Skils() {
-  return (
-    <section id='skils' className='skils'>
-      <PageHeaderContent 
-        headerText = 'My Skills'
-        icon = {<BsInfoCircleFill size={40} />}
-      />
-    </section>
-  )
+    return (
+        <section id="skils" className="skils">
+            <PageHeaderContent
+                headerText="My Skills"
+                icon={<BsInfoCircleFill size={40} />}
+            />
+            <div className="skils__content-wrapper">
+                {skilData.map((item, i) => (
+                    <div
+                        key={i}
+                        className="skils__content-wrapper__inner-content"
+                    >
+                        <Animate
+                            play
+                            duration={1}
+                            delay={0.3}
+                            start={{
+                                transform: "translateX(-200px)",
+                            }}
+                            end={{
+                                transform: "translateX(0px)",
+                            }}
+                        >
+                            <h3 className="skils__content-wrapper__inner-content__category-text">
+                                {item.label}
+                            </h3>
+
+                            <div className="skils__content-wrapper__inner-content__progressbar-container">
+                                {
+                                item.data.map((skilItem, j) => (
+                                    <AnimateKeyframes
+                                        play
+                                        duration={1}
+                                        keyframes={["opacity: 1", "opacity: 0"]}
+                                        iterationCount="1"
+                                    >
+                                      <div className="progressbar-wrapper" key={j}>
+                                        <p>{skilItem.skillName}</p>
+                                        <Line
+                                          percent={skilItem.porcetage}
+                                          strokeWidth='2'
+                                          strokeColor='var(--theme-main-color)'
+                                          trailWidth='2'
+                                          strokeLinecap="square"
+                                        />
+                                      </div>
+                                    </AnimateKeyframes>
+                                ))
+                                }
+                            </div>
+                        </Animate>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
 
-export default Skils
+export default Skils;
---
+++ src/Containers/Skils/utils.js
@@ -0,0 +1,49 @@
+export const skilData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                porcetage: "90",
+            },
+            {
+                skillName: "CSS",
+                porcetage: "70",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                porcetage: "70",
+            },
+            {
+                skillName: "REACT",
+                porcetage: "60",
+            },
+        ],
+    },
+    {
+        label: "BECK END",
+        data: [
+            {
+                skillName: "NODE.JS",
+                porcetage: "40",
+            },
+            {
+                skillName: "PYTHON",
+                porcetage: "30",
+            },
+        ],
+    },
+    {
+        label: "DATABASE",
+        data: [
+            {
+                skillName: "SQL",
+                porcetage: "40",
+            },
+            {
+                skillName: "MONGO DB",
+                porcetage: "20",
+            },
+        ],
+    },
+];
